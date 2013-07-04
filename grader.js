81c513ce618d84545ff32722fb0299122b8e2239
Ubuntu <ubuntu@ip-172-31-16-85.us-west-2.compute.internal>
2013-07-04 22:50:01 +0000

    Initial grader.js and checks.json

---
+++ grader.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
+if(require.main == module) {
+    program
+	.option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFIL_DEFAULT)
+	.option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+	.parse(process.argv);
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
