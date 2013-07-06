14898d0bf5e55e22dc01b20c2eadf65fb3f9f6ad
Ubuntu <ubuntu@ip-172-31-16-85.us-west-2.compute.internal>
2013-07-06 22:42:38 +0000

    Remote grader.js

--- grader.js
+++ grader.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+var util = require('util');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://evening-dusk-5911.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -17,6 +20,19 @@ var assertFileExists = function(infile) {
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var buildfn = function(checksfile) {
+    var responseHandler = function(result, response) {
+	if(result instanceof Error) {
+	    console.error('Error: ' + util.format(response.message));
+	} else {
+	    console.error('Writing temp.html...');
+	    fs.writeFileSync("temp.html", result);
+	    processHtml("temp.hmtl", checksfile);
+	}
+    };
+    return responseHandler;
 };
 
 var loadChecks = function(checksfile) {
@@ -34,8 +50,15 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var processHtml = function(htmlfile, checksfile) {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+
 var clone = function(fn) {
-    // http://stackoverflow.com/a/6772648
+    // workaround for commander issue (http://stackoverflow.com/a/6772648)
     return fn.bind({});
 };
 
@@ -43,10 +66,15 @@ if(require.main == module) {
     program
 	.option('-c, --checks <checks_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Url of index.html', null, URL_DEFAULT)
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+	console.error('Attempting to get from url...');
+	rest.get(util.format(program.url + '/index.html')).on('complete', buildfn(program.checks));
+    } else if(program.file) {
+	console.error('Attempting to read from file...');
+	processHtml(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
